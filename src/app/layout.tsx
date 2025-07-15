6efad56aec842e8c171dd04dbdfa242833b512ee
Pradeep-Chandra-17 <pradeepchandragajendra@gmail.com>
2025-07-15 19:03:51 +0530

    Added OG Image, robots

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,13 +8,26 @@ import "react-toastify/dist/ReactToastify.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "SchooLama AI LMS",
+  title: "SchooLama - AI-powered LMS for Modern Education",
   description:
     "SchooLama is a free AI-powered LMS built for intelligent learning and education management. Start your trial today and experience smart analytics, personalized content, and streamlined course creation for modern educators.",
-  alternates: {
-    canonical: "https://www.schoolama.studio",
+  openGraph: {
+    title: "SchooLama - AI-Powered Learning Management System",
+    description: "Experience intelligent learning with AI-powered features, analytics, and free LMS tools. Get started with your trial today!",
+    url: "https://www.schoolama.studio",
+    siteName: "SchooLama",
+    images: [
+      {
+        url: "https://www.schoolama.studio/og-image.jpg", // Update this path
+        width: 1200,
+        height: 630,
+        alt: "SchooLama Open Graph Banner",
+      },
+    ],
+    type: "website",
   },
-};
+}
+
 
 export default function RootLayout({
   children,
---
+++ src/app/robots.ts
@@ -0,0 +1,14 @@
+import { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/admin/', '/list/', 'parent', 'student', 'teacher', '/api'], // add your private routes here
+      },
+    ],
+    sitemap: 'https://www.schoolama.studio/sitemap.xml',
+  }
+}
