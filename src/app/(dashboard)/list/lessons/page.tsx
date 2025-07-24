d686fb86c64daa8bd88765d1863299a0925a4c49
Pradeep-Chandra-17 <pradeepchandragajendra@gmail.com>
2025-07-24 17:11:52 +0530

    Fixing Lessons

--- src/app/(dashboard)/list/lessons/page.tsx
+++ src/app/(dashboard)/list/lessons/page.tsx
@@ -53,6 +53,7 @@ const LessonListPage = async ({
       key={item.id}
       className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight"
     >
+      <td className="flex items-center gap-4 p-4">{item.name}</td>
       <td className="flex items-center gap-4 p-4">{item.subject.name}</td>
       <td>{item.class.name}</td>
       <td className="hidden md:table-cell">
