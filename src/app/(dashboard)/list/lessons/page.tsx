4bd0bfa7bdbdf4125af2c2bb43ee3bb27ab98925
Pradeep-Chandra-17 <pradeepchandragajendra@gmail.com>
2025-07-24 17:14:42 +0530

    Fixing row display in Lessons

--- src/app/(dashboard)/list/lessons/page.tsx
+++ src/app/(dashboard)/list/lessons/page.tsx
@@ -54,7 +54,7 @@ const LessonListPage = async ({
       className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight"
     >
       <td className="flex items-center gap-4 p-4">{item.name}</td>
-      <td className="flex items-center gap-4 p-4">{item.subject.name}</td>
+      <td>{item.subject.name}</td>
       <td>{item.class.name}</td>
       <td className="hidden md:table-cell">
         {item.teacher.name + " " + item.teacher.surname}
