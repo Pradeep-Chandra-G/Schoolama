5f880bd0588eb379b6b1475564dab548bca5a17f
Pradeep-Chandra-17 <pradeepchandragajendra@gmail.com>
2025-08-24 22:27:35 +0530

    Fixed customProgress and customProgressBar classes

--- src/app/pradeep/_components/portfolio/Portfolio.tsx
+++ src/app/pradeep/_components/portfolio/Portfolio.tsx
@@ -46,13 +46,13 @@ const Single = ({ item }: { item: PortfolioItem }) => {
   const y = useTransform(scrollYProgress, [0, 1], [-300, 300]);
 
   return (
-    <section >
+    <section>
       <div className="container">
         <div className="wrapper">
           <div className="imageContainer" ref={ref}>
             <img src={item.img} alt="" />
           </div>
-          <motion.div className="textContainer" style={{y}}>
+          <motion.div className="textContainer" style={{ y }}>
             <h2>{item.title}</h2>
             <p>{item.desc}</p>
             <button>See Demo</button>
@@ -78,9 +78,12 @@ const Portfolio = () => {
 
   return (
     <div className="portfolio" ref={ref}>
-      <div className="progress">
+      <div className="customProgress">
         <h1>Featured Works</h1>
-        <motion.div style={{ scaleX }} className="progressBar"></motion.div>
+        <motion.div
+          style={{ scaleX }}
+          className="customProgressBar"
+        ></motion.div>
       </div>
       {items.map((item) => (
         <Single item={item} key={item.id} />
