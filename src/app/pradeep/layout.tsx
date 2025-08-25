ffae4867858bebecc24e8253b426f7f8190dbbc8
Pradeep-Chandra-17 <pradeepchandragajendra@gmail.com>
2025-08-25 12:28:50 +0530

    Fixed portfolio styles

--- src/app/pradeep/layout.tsx
+++ src/app/pradeep/layout.tsx
@@ -1,11 +1,13 @@
-// app/pradeep/layout.tsx
+// src/app/pradeep/layout.tsx
 import { dmSans } from "@/lib/fonts";
+import "./portfolio.modules.scss";
 
 export default function PradeepLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  // This layout wraps only /pradeep routes
-  return <div className={dmSans.className}>{children}</div>;
+  return (
+    <div className={`pradeep-portfolio ${dmSans.className}`}>{children}</div>
+  );
 }
