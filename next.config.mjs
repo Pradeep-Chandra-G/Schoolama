5f4a198579f1e28cdb2eb9959fe973b469aebfe5
Pradeep-Chandra-17 <pradeepchandragajendra@gmail.com>
2025-11-29 12:01:18 +0530

    Added permanent redirect for home via next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,12 +3,19 @@ const nextConfig = {
   images: {
     domains: ["developers.google.com"],
     remotePatterns: [
-      { hostname: 
-        "images.pexels.com"
-      }
-      ,{hostname: "res.cloudinary.com"}
+      { hostname: "images.pexels.com" },
+      { hostname: "res.cloudinary.com" },
     ],
-    
+  },
+
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/home",
+        permanent: true, // 308 redirect
+      },
+    ];
   },
 };
 
